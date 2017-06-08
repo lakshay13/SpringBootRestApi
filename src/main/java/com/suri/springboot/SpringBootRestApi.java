e0e0a0f4bb0a246b00dc0ca378c13cf055d56906
Lakshay Suri <lakshay.suri@gemalto.com>
2017-06-08 14:07:15 +0200

    feature(springboot-application-run): Committing spring boot class created.

---
+++ src/main/java/com/suri/springboot/SpringBootRestApi.java
@@ -0,0 +1,15 @@
+package com.suri.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lakshay13@gmail.com
+ */
+@SpringBootApplication
+public class SpringBootRestApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApi.class, args);
+    }
+}
